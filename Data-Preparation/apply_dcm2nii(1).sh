b7a12ab12ac1ec3b616adcadfcfcc8239555c7f3
Hendrale Gresseau <hendralegresseau@gmail.com>
2025-05-12 14:14:14 -0400

    Create apply_dcm2nii(1).sh

---
+++ Data-Preparation/apply_dcm2nii(1).sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+participant_dir=${1}
+participant=${2}
+
+# Process anat files
+dcm2niix -z y -f "sub-${participant}_acq-MPRAGE0p9_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE_HR"
+
+dcm2niix -z y -f "sub-${participant}_acq-MPRAGE1p5_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE 1.5mm"
+
+# Process perf files
+dcm2niix -z y -f "sub-${participant}_acq-pcaslDEe1_run-1_asl" -o "$participant_dir/perf" "$participant_dir/perf/ep2d_DE_pCASL_LOFTmod_XA30 TE10 TE30"
+
+# Process M0 scan
+dcm2niix -z y -f "sub-${participant}_acq-pcasl_run-1_m0scan" -o "$participant_dir/perf" "$participant_dir/perf/ep2d_DE_pCASL_LOFTmod_XA30 M0"
