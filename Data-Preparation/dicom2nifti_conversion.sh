d0c745b7d29c6c9b392cd96c44d29c2a63d13ec4
Hendrale Gresseau <hendralegresseau@gmail.com>
2025-05-15 17:34:09 -0400

    Update dicom2nifti_conversion.sh

--- Data-Preparation/dicom2nifti_conversion.sh
+++ Data-Preparation/dicom2nifti_conversion.sh
@@ -6,26 +6,26 @@
 # This script compresses raw data (DICOM) into usable files (NIfTI)
 
 # Define the base path
-path=/NAS/home/s_sanami/Documents/covirm_data_processing/Hendrale/
+path=/insert/your/base/path/
 
 # Navigate to the working directory
 cd $path
 
-participants=("016" "017" "018" "019" "020" "021" "023" "024" "025" "026" "030" "031")
+participants= #insert participants identification number (ID) 
 
-# Loop over each participant (COVIRM-016 to COVIRM-030)
+# Loop over each participant
 for participant in "${participants[@]}"
 do
-    participant_dir="data/COVIRM-${participant}"
+    participant_dir="path/to/your/participant/folder"
     echo "Processing participant $participant"
 
     # Call apply_dcm2nii.sh with the full participant directory
-    /NAS/home/s_sanami/Documents/covirm_data_processing/Hendrale/apply_dcm2nii.sh ${participant_dir} ${participant}
+    /path/to/the/conversion/script/apply_dcm2nii.sh ${participant_dir} ${participant}
 
-    # Rename e1 and e2 files to use DEe1 and DEe2 instead of _e1 and _e2
-    mv "$participant_dir/perf/sub-${participant}_acq-pcaslDEe1_run-1_asl_e1.nii.gz" "$participant_dir/perf/sub-${participant}_acq-pcaslDEe1_run-1_asl.nii.gz"
-    mv "$participant_dir/perf/sub-${participant}_acq-pcaslDEe1_run-1_asl_e2.nii.gz" "$participant_dir/perf/sub-${participant}_acq-pcaslDEe2_run-1_asl.nii.gz"
-    mv "$participant_dir/perf/sub-${participant}_acq-pcaslDEe1_run-1_asl_e1.json" "$participant_dir/perf/sub-${participant}_acq-pcaslDEe1_run-1_asl.json" 
-    mv "$participant_dir/perf/sub-${participant}_acq-pcaslDEe1_run-1_asl_e2.json" "$participant_dir/perf/sub-${participant}_acq-pcaslDEe2_run-1_asl.json"
+    # Include the following to rename the _e1 and _e2 files of each participant such that they use _DEe1 and _DEe2 instead (useful for subsequent steps)
+    mv "path/to/the/file/_e1.nii.gz" "path/to/the/renamed/file/_DEe1.nii.gz" # The first term represent the current file's name. The second term is the new name you would for it.
+    mv "path/to/the/file/_e2.nii.gz" "path/to/the/renamed/file/_DEe2.nii.gz" # The first term represent the current file's name. The second term is the new name you would for it.
+    mv "path/to/the/file/_e1.json" "path/to/the/renamed/file/_DEe1.json" # The first term represent the current file's name. The second term is the new name you would for it.
+    mv "path/to/the/file/_e2.json" "path/to/the/renamed/file/_DEe2.json" # The first term represent the current file's name. The second term is the new name you would for it.
 
 done
