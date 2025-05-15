6af501dac8a7e80bf85db5e520616b19453632d3
Hendrale Gresseau <hendralegresseau@gmail.com>
2025-05-15 17:56:03 -0400

    Update apply_dcm2nii.sh

--- Data-Preparation/apply_dcm2nii.sh
+++ Data-Preparation/apply_dcm2nii.sh
@@ -6,12 +6,12 @@
 participant_dir=${1}
 participant=${2}
 
-# Process anat files
+# Process anatomy (anat) files
 dcm2niix -z y -f "sub-${participant}_acq-MPRAGE0p9_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE_HR"
 
 dcm2niix -z y -f "sub-${participant}_acq-MPRAGE1p5_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE 1.5mm"
 
-# Process perf files
+# Process perfusion (perf) files
 dcm2niix -z y -f "sub-${participant}_acq-pcaslDEe1_run-1_asl" -o "$participant_dir/perf" "$participant_dir/perf/ep2d_DE_pCASL_LOFTmod_XA30 TE10 TE30"
 
 # Process M0 scan
