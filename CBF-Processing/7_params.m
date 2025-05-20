afc22e8b08893448f4d3aea6994e3ac9e54f73bd
Hendrale Gresseau <hendralegresseau@gmail.com>
2025-05-20 16:55:29 -0400

    Update 7_params.m

--- CBF-Processing/7_params.m
+++ CBF-Processing/7_params.m
@@ -2,32 +2,28 @@
 % Approved by Safa Sanami
 
 % Define the directory where the file will be saved
-data_path = '/NAS/home/s_sanami/Documents/covirm_data_processing/Hendrale/data/';
+data_path = '/path/to/data/';
 
 % Define the content for the file (excluding repeats)
 params = {
     '--casl'
-    '--t1=1.4'
-    '--t1b=1.65'
-    '--tau=1.8'
-    '--bat=1.3'
-    '--pld=1.3'
+    '--t1= 
+    '--t1b= 
+    '--tau= 
+    '--bat= 
+    '--pld= 
 };
 
 % List of subjects with their corresponding repeats
 subjects_repeats = {
-    '016', 14;
-    '017', 14;
-    '018', 14;
-    '019', 14;
-    '020', 12;
-    '021', 14;
-    '023', 14;
-    '024', 14;
-    '025', 14;
-    '026', 13;
-    '030', 14;
-    '031', 11
+    'participant ID', number of repeats;
+    'participant ID', number of repeats;
+    'participant ID', number of repeats;
+    .
+    .
+    .
+    'participant ID', number of repeats;
+    'participant ID', number of repeats
 };
 
 % Loop through each subject and generate the params.txt file
@@ -39,7 +35,7 @@ for z = 1:size(subjects_repeats, 1)
   params_with_repeats = [params; {['--repeats=', num2str(repeats)]}];
 
   % Define the file path to save the params.txt
-  file_path = fullfile(data_path, ['COVIRM-', subj], 'params.txt');
+  file_path = fullfile(data_path, ['path/to/subject/directory], 'params.txt');
   
   % Open the file for writing
   fileID = fopen(file_path, 'w');
