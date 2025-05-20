bca85681acaf6229b7788e0269e386772be8d834
Hendrale Gresseau <hendralegresseau@gmail.com>
2025-05-20 15:18:37 -0400

    Update apply_dcm2nii.sh

--- Data-Preparation/apply_dcm2nii.sh
+++ Data-Preparation/apply_dcm2nii.sh
@@ -7,19 +7,21 @@ participant_dir=${1}
 participant=${2}
 
 # Process anatomy (anat) files
+
 # -z y: Compresses the output NIfTI file into .nii.gz. y means yes.
 # -f: output filename:
-  # ANAT: _acq-MPRAGE0p9_run-1_T1w (High Resolution T1-Weighted Image)
-  #       _acq-MPRAGE1p5_run-1_T1w (Low Resolution T1-Weighted Image)
-  # PERF: _acq-pcaslDEe1_run-1_asl (pCASL/BOLD)
-  #       _acq-pcasl_run-1_m0scan (M0)
+  # ANAT: high resolution t1w dcm files, typically '_acq-MPRAGE0p9_run-1_T1w'
+  #       low resolution t1w dcm files, typically '_acq-MPRAGE1p5_run-1_T1w'
+  # PERF: pCASL/BOLD dcm files, typically '_acq-pcaslDEe1_run-1_asl'
+  #       M0 scan dcm files, typically '_acq-pcasl_run-1_m0scan (M0)'
 # -o: directory to save the output file.
-dcm2niix -z y -f "sub-${participant}_acq-MPRAGE0p9_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE_HR"
 
-dcm2niix -z y -f "sub-${participant}_acq-MPRAGE1p5_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE 1.5mm"
+dcm2niix -z y -f "filnename format (high res t1w)" -o "output directory" "path to dcm files, typically 'MPRAGE_HR'"
+
+dcm2niix -z y -f "filnename format (low res t1w)" -o "output directory" "path to dcm files, typically 'MPRAGE 1.5mm'"
 
 # Process perfusion (perf) files
-dcm2niix -z y -f "sub-${participant}_acq-pcaslDEe1_run-1_asl" -o "$participant_dir/perf" "$participant_dir/perf/ep2d_DE_pCASL_LOFTmod_XA30 TE10 TE30"
+dcm2niix -z y -f "filename format (pCASL/BOLD)" -o "output directory" "path to dcm files, typically '_LOFTmod_XA30 TE10 TE30'"
 
 # Process M0 scan
-dcm2niix -z y -f "sub-${participant}_acq-pcasl_run-1_m0scan" -o "$participant_dir/perf" "$participant_dir/perf/ep2d_DE_pCASL_LOFTmod_XA30 M0"
+dcm2niix -z y -f "filename format (M0)" -o "output directory" "path to dcm files, typically '_LOFTmod_XA30 M0'"
