b6f6189d31b7dbb77f1b4318ee95201a9c1d4eee
Hendrale Gresseau <hendralegresseau@gmail.com>
2025-05-20 15:05:53 -0400

    Update apply_dcm2nii.sh

--- Data-Preparation/apply_dcm2nii.sh
+++ Data-Preparation/apply_dcm2nii.sh
@@ -7,6 +7,13 @@ participant_dir=${1}
 participant=${2}
 
 # Process anatomy (anat) files
+# -z y: Compresses the output NIfTI file into .nii.gz. y means yes.
+# -f: output filename:
+  # ANAT: _acq-MPRAGE0p9_run-1_T1w (High Resolution T1-Weighted Image)
+  #       _acq-MPRAGE1p5_run-1_T1w (Low Resolution T1-Weighted Image)
+  # PERF: _acq-pcaslDEe1_run-1_asl (pCASL/BOLD)
+  #       _acq-pcasl_run-1_m0scan (M0)
+# -o: directory to save the output file.
 dcm2niix -z y -f "sub-${participant}_acq-MPRAGE0p9_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE_HR"
 
 dcm2niix -z y -f "sub-${participant}_acq-MPRAGE1p5_run-1_T1w" -o "$participant_dir/anat" "$participant_dir/anat/MPRAGE 1.5mm"
